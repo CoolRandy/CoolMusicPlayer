bca6f60cccbaca1dec859e7ce328c6ae51537614
coolrandy <lvhaojack@gmail.com>
2016-03-02 10:57:03 +0800

    修改一些问题

---
+++ mylibrary/src/main/java/com/android/volley/toolbox/OkHttpStack.java
@@ -0,0 +1,31 @@
+package com.android.volley.toolbox;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by admin on 2016/3/2.
+ */
+public class OkHttpStack extends HurlStack {
+
+    private OkHttpClient okHttpClient;
+
+    public OkHttpStack(){
+
+        this(new OkHttpClient());
+    }
+
+    public OkHttpStack(OkHttpClient okHttpClient){
+        this.okHttpClient = okHttpClient;
+    }
+
+    @Override
+    protected HttpURLConnection createConnection(URL url) throws IOException {
+        OkUrlFactory okUrlFactory = new OkUrlFactory(okHttpClient);
+        return okUrlFactory.open(url);
+    }
+}
